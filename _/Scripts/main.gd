ebc1a868a1085565b93b874a1143140803e4ed9d
miler71 <jeaemile50@gmail.com>
2026-05-19 16:31:34 +0200

    Utilisation de Array
    remove at et append(ajout)

--- _/Scripts/main.gd
+++ _/Scripts/main.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
 func _ready():
-	var character_height = randi_range(130, 210)
-	print("Your character is : " + str(character_height))
+	var items: Array[String] = ["Potion", "Feather", "Stolen harp"]
+	items[1] = "Smelly Sock"
+	items[2] = "Staff"
+	
+	items.remove_at(1)
+	items.append("Overpowered sword")
