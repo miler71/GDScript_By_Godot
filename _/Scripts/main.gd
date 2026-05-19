58a9cdfc61c71b3cb8191117c54e454b095a0150
miler71 <jeaemile50@gmail.com>
2026-05-19 15:56:41 +0200

    Calculs with 2 func
    func _ready and func add

--- _/Scripts/main.gd
+++ _/Scripts/main.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-func _input(event):
-	if event.is_action_pressed("my_action"):
-		jump()
+func _ready():
+	var result = add(3, 5)
+	result = add(result, 10)
+	print (result)
 
-func jump():
-	
-	
-	print("Jump!")
+func add(num1, num2):
+	var result = num2 * num1
+	return result
