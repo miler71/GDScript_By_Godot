b00df017104f0e82559c270659713052c972716f
miler71 <jeaemile50@gmail.com>
2026-05-19 16:08:25 +0200

    function aléatoires
    si le roll =  randf() et que roll est plus petit ou = a 0.8

--- _/Scripts/main.gd
+++ _/Scripts/main.gd
@@ -1,10 +1,8 @@
 extends Node2D
 
 func _ready():
-	var result = add(3, 5)
-	result = add(result, 10)
-	print (result)
-
-func add(num1, num2):
-	var result = num2 * num1
-	return result
+	var roll = randf()
+	if roll <= 0.8:
+		print("You found in common items")
+	else:
+		print("You not found in commons items")
