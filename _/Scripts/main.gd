f996e6d5df85a34d327df8c0d5f0707d590c9f45
miler71 <jeaemile50@gmail.com>
2026-05-19 14:49:31 +0200

    InputMap
    Change color on text with input Space

--- _/Scripts/main.gd
+++ _/Scripts/main.gd
@@ -5,3 +5,11 @@ extends Node2D
 func _ready() -> void:
 	$Label.text = "Hello, World !"
 	$Label.modulate = Color.DARK_SLATE_BLUE
+
+func _input(event):
+	if event.is_action_pressed("change_color"):
+		$Label.modulate = Color.DARK_RED
+	
+	if event.is_action_released("change_color"):
+		$Label.modulate = Color.DARK_SLATE_BLUE
+	
