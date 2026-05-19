e3c3a74b5f03abd2bae35eedf8c0635264c941c0
miler71 <jeaemile50@gmail.com>
2026-05-19 15:32:47 +0200

    Variables
    Addding an export in the inspector

--- _/Scripts/main.gd
+++ _/Scripts/main.gd
@@ -1,20 +1,7 @@
 extends Node2D
 
-var health = 100
+@export var damage := 15
 
 
-func _input(event):
-	if event.is_action_pressed("my_action"):
-		health -= 20 
-		 
-		print(health)
-		
-		if health <= 0:
-			health = 0
-			print("YOU DIED !")
-		elif health < 50: 
-			print("YOU ARE INJURED !")
-		else: 
-			print("You are in health !")
-		
-		
+func _ready(): 
+	pass
